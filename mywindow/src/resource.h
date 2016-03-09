2fd138126234852710fcd03814571a1100074d0c
zhaojian770627 <zhaojian770627@163.com>
2016-03-09 22:07:57 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

---
+++ mywindow/src/resource.h
@@ -0,0 +1,16 @@
+#ifndef RESOURCE_H_INCLUDED
+#define RESOURCE_H_INCLUDED
+
+#define MYMENU1   11100
+
+#define IDM_New   101
+#define IDM_Open  102
+#define IDM_Save  103
+#define IDM_Exit  104
+#define IDM_Cut   201
+#define IDM_Copy  202
+#define IDM_Paste 203
+#define IDM_About 301
+
+
+#endif // RESOURCE_H_INCLUDED
