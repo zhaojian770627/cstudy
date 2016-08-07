73e6cc9a168e6e05845ef310482ffdfb019e0a4d
zhaojianc <zhaojianc@yonyou.com>
2016-08-07 12:55:13 +0800

    add
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- MemManage/src/MemManage.cpp
+++ MemManage/src/MemManage.cpp
@@ -5,20 +5,27 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-#include "DynMem.h"
+//#include "DynMem.h"
+
 #include <iostream>
+#include "RelMem.h"
+
 using namespace std;
 
 int main() {
-	DynMem dynMem;
-	void *p1=dynMem.NewPtr(16);
-	cout <<"p1:"<< p1 << endl;
-	void *p2=dynMem.NewPtr(16);
-	cout <<"p2:"<< p2 << endl;
-	void *p3=dynMem.NewPtr(16);
-	cout <<"p3:"<< p3 << endl;
-	dynMem.FreePtr(p1);
-	dynMem.FreePtr(p3);
-	dynMem.FreePtr(p2);
+	/*
+	 DynMem dynMem;
+	 void *p1=dynMem.NewPtr(16);
+	 cout <<"p1:"<< p1 << endl;
+	 void *p2=dynMem.NewPtr(16);
+	 cout <<"p2:"<< p2 << endl;
+	 void *p3=dynMem.NewPtr(16);
+	 cout <<"p3:"<< p3 << endl;
+	 dynMem.FreePtr(p1);
+	 dynMem.FreePtr(p3);
+	 dynMem.FreePtr(p2);
+	 */
+	// 测试可重定位的内存管理类
+	RelMem relMem;
 	return 0;
 }
--- MemManage/src/RelMem.cpp
+++ MemManage/src/RelMem.cpp
@@ -7,22 +7,34 @@
 
 #include "RelMem.h"
 
-RelMem::RelMem(int nMax,int eps,int wSize):DynMem(eps,wSize) {
-	Header *head=FreeList;
-	Footer *foot=FootOf(head);
+RelMem::RelMem(int nMax, int eps, int wSize) :
+		DynMem(eps, wSize) {
+	Header *head = FreeList;
+	Footer *foot = FootOf(head);
 	// nMax 太大或太小时，进行调整
-	if(nMax<MinMasters||nMax>(head->size-MinEpsilon)/4)
-		nMax=MinMasters;
-	nMasters=nMax;
-	head->size-=nMasters*sizeof(void*);
-	Footer *NewFoot=FootOf(head);
-	Header *NewHead=(Header*)(NewFoot+1);
-	head=(Header*)(foot+1);
-	*NewFoot=*foot;
-	*NewHead=*head;
-	masters=(void**)(NewHead+1);
+	if (nMax < MinMasters || nMax > (head->size - MinEpsilon) / 4)
+		nMax = MinMasters;
+	nMasters = nMax;
+	head->size -= nMasters * sizeof(void*);
+	Footer *NewFoot = FootOf(head);
+	Header *NewHead = (Header*) (NewFoot + 1);
+	head = (Header*) (foot + 1);
+	*NewFoot = *foot;
+	*NewHead = *head;
+	masters = (void**) (NewHead + 1);
 	// 初始时，所有控制指针都为空闲
-	for(register int i=0;i<nMasters;++i)
-		masters[i]=0;
-	MasIdx=0;		// 第一个自由的master指针
+	for (register int i = 0; i < nMasters; ++i)
+		masters[i] = 0;
+	MasIdx = 0;		// 第一个自由的master指针
+}
+
+void RelMem::MoveBlock(Header* src, Header *dest) {
+	register int bytes=src->size;
+	register char *from=(char*)src;
+	register char *to=(char*)dest;
+
+	while(bytes--)
+		*to++=*from++;
+	FootOf(dest)->uLink=dest;
+	*(dest->master)=BlockOf(dest);
 }
