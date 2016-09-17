b54232c2f3c00456f8a0c1cbd95da805797bad5e
zhaojian <zhaojian770627@163.com>
2016-09-17 21:44:43 +0800

    add
    Signed-off-by: zhaojian <zhaojian770627@163.com>

---
+++ linuxmanaual/src/time/show_time.c
@@ -0,0 +1,33 @@
+/*
+ * show_time.c
+ *
+ *  Created on: Sep 17, 2016
+ *      Author: root
+ */
+
+#include <time.h>
+#include <locale.h>
+#include "../lib/tlpi_hdr.h"
+
+#define BUF_SIZE 200
+
+int main(int argc, char *argv[]) {
+	time_t t;
+	struct tm *loc;
+	char buf[BUF_SIZE];
+
+	if (setlocale(LC_ALL, "") == NULL)
+		errExit("setlocale");	// Use locale settings in conversions
+	t = time(NULL);
+	printf("ctime() of time() value is: %s", ctime(&t));
+
+	loc = localtime(&t);
+	if (loc == NULL)
+		errExit("localtime");
+	printf("asctime() of time() value is: %s", asctime(loc));
+
+	if (strftime(buf, BUF_SIZE, "%A,%d %B %Y, %H:%M:%S %Z", loc) == 0)
+		fatal("strftime returned 0");
+	printf("strftime() of local time is:%s\n", buf);
+	exit(EXIT_SUCCESS);
+}
