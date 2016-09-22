f081b2f587d41cd994606966ea21e2086a175995
zhaojian <zhaojian770627@163.com>
2016-09-22 22:25:51 +0800

    add
    Signed-off-by: zhaojian <zhaojian770627@163.com>

--- linuxmanaual/src/study/e11/e1.c
+++ linuxmanaual/src/study/e11/e1.c
@@ -5,7 +5,10 @@
  *      Author: root
  */
 #include <dirent.h>
+#include <fcntl.h>
 #include "../../lib/tlpi_hdr.h"
+
+#define CHARMAX 1024
 
 int main(int argc, char *argv[]) {
 	if (argc < 2 || strcmp(argv[1], "--help") == 0)
@@ -14,6 +17,10 @@ int main(int argc, char *argv[]) {
 	DIR *dirp;
 	struct dirent *dp;
 	char dirpath[] = "/proc";
+	char pathName[CHARMAX];
+	char buffer[CHARMAX];
+	FILE *input;
+
 	dirp = opendir(dirpath);
 	if (dirp == NULL) {
 		errMsg("opendir failed on '%s'", dirpath);
@@ -26,7 +33,30 @@ int main(int argc, char *argv[]) {
 
 		if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
 			continue;
-		printf("%s\n", dp->d_name);
+		int i = strtol(dp->d_name, NULL, 10);
+		if (i != 0L) {
+
+			sprintf(pathName, "/proc/%s/status", dp->d_name);
+			pathName[strlen(pathName)] = '\0';
+
+			input = fopen(pathName, "r");
+			if (input == NULL) {
+				printf("open %s error", pathName);
+				continue;
+			}
+
+			while (fgets(buffer, CHARMAX, input) != NULL) {
+				if (strncmp(buffer, "Name:", 5) == 0){
+					printf("%s", buffer);
+					break;
+				}
+			}
+
+			printf("open %s \n", pathName);
+
+			if (fclose(input) != 0)
+				printf("close %s error", pathName);
+		}
 	}
 
 	if (errno != 0)
