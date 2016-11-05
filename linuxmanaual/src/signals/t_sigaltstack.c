2442da215e64cfb151b976b90c3cdd74caed0dd4
zhaojian <zhaojian770627@163.com>
2016-11-05 23:00:50 +0800

    add
    Signed-off-by: zhaojian <zhaojian770627@163.com>

--- linuxmanaual/src/signals/t_sigaltstack.c
+++ linuxmanaual/src/signals/t_sigaltstack.c
@@ -1,10 +1,10 @@
 #define _GNU_SOURCE
 #include <string.h>
-# include <signal.h>
-#include "../lib/tlip_hdr.h"
+#include <signal.h>
+#include "../lib/tlpi_hdr.h"
 
-static void
-sigsegvHandler(int sig)
+
+static void sigsegvHandler(int sig)
 {
   int x;
   /* UNSAFE:this handler uses non-async-signal-safe functions  */
@@ -15,7 +15,8 @@ sigsegvHandler(int sig)
   _exit(EXIT_FAILURE);		/* Can't return after SIGSEGV */
 }
 
-static void overflowStack(int callNum) /* A recursive function that overflows the stack */
+static void overflowStack(int callNum) /* A recursive function that
+					  overflows the stack */
 {
   char a[100000];		/* Make this stack frame large */
   printf("Call %4d- top of stack near %10p\n",callNum,&a[0]);
@@ -23,5 +24,25 @@ static void overflowStack(int callNum) /* A recursive function that overflows th
 }
 int main(int argc,char *argv[])
 {
+  stack_t sigstack;
+  struct sigaction sa;
+  int j;
 
+  printf("Top of standard stack is near %10p\n",(void*)&j);
+  /* Allocate alternate stack and inform kernel of iss existence */
+  sigstack.ss_sp=malloc(SIGSTKSZ);
+  if(sigstack.ss_sp==NULL)
+    errExit("malloc");
+  sigstack.ss_size=SIGSTKSZ;
+  sigstack.ss_flags=0;
+  if(sigaltstack(&sigstack,NULL)==-1)
+    errExit("sigaltstack");
+  printf("Alternate stack is at %10p-%p\n",
+	 sigstack.ss_sp,(char*)sbrk(0)-1);
+  sa.sa_handler=sigsegvHandler;	/* Establish handler for SIGSEGV */
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags=SA_ONSTACK;	/* Handler uses alternate stack*/
+  if(sigaction(SIGSEGV,&sa,NULL)==-1)
+    errExit("sigaction");
+  overflowStack(1);
 }
