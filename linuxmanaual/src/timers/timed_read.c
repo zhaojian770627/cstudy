bd5794e6363fbf01a8e8679df77c15aa86cf149c
zhaojian <zhaojian770627@163.com>
2016-11-21 20:11:45 +0800

    add
    Signed-off-by: zhaojian <zhaojian770627@163.com>

---
+++ linuxmanaual/src/timers/timed_read.c
@@ -0,0 +1,43 @@
+#include <signal.h>
+#include "../lib/tlpi_hdr.h"
+
+#define BUF_SIZE 200
+
+/* SIGALRM handler:interrupts blocked system call */
+static void handler(int sig)
+{
+  printf("Caught signal\n");
+}
+
+int main(int argc,char *argv[]){
+  struct sigaction sa;
+  char buf[BUF_SIZE];
+  ssize_t numRead;
+  int savedErrno;
+
+  if(argc>1 && strcmp(argv[1],"--help")==0)
+    usageErr("%s [num-secs [restart-flag]]\n",argv[0]);
+
+  sa.sa_flags=(argc>2)?SA_RESTART:0;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_handler=handler;
+  if(sigaction(SIGALRM,&sa,NULL)==-1)
+    errExit("sigaction");
+  alarm((argc>1)?getInt(argv[1],GN_NONNEG,"num-secs"):10);
+
+  numRead=read(STDIN_FILENO,buf,BUF_SIZE-1);
+  savedErrno=errno;		/* In case alarm() changes it */
+  alarm(0);			/* Ensure timer is turned off */
+  errno=savedErrno;
+  /* Determine result of read */
+  if(numRead==-1){
+    if(errno==EINTR)
+      printf("Read timed out\n");
+    else
+      errMsg("read");
+  }else{
+    printf("Successful read (%ld bytes):%.*s",
+	   (long)numRead,(int)numRead,buf);
+  }
+  exit(EXIT_SUCCESS);
+}
