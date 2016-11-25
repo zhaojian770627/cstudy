f78f303819a5fc2a1b4e73d7b654af003db72c51
zhaojian <zhaojian770627@163.com>
2016-11-25 22:44:55 +0800

    add
    Signed-off-by: zhaojian <zhaojian770627@163.com>

---
+++ linuxmanaual/src/timers/demo_timerfd.c
@@ -0,0 +1,50 @@
+#include <sys/timerfd.h>
+#include <time.h>
+#include <stdint.h>		 /* Definition of unit64_t */
+#include "itimerspec_from_str.h" /* Declares itimerspecFromStr */
+#include "../lib/tlpi_hdr.h"
+
+int main(int argc,char *argv[])
+{
+  struct itimerspec ts;
+  struct timespec start,now;
+  int maxExp,fd,secs,nanosecs;
+  uint64_t numExp,totalExp;
+  ssize_t s;
+
+  if(argc<2 || strcmp(argv[1],"--help")==0)
+    usageErr("%s secs[/nsecs][:int-secs[/int-nsecs]][max-exp]\n",argv[0]);
+
+  itimerspecFromStr(argv[1],&ts);
+  maxExp=(argc>2)?getInt(argv[2],GN_GT_0,"max-exp"):1;
+  fd=timerfd_create(CLOCK_REALTIME,0);
+  if(fd==-1)
+    errExit("timerfd_create");
+
+  if(timerfd_settime(fd,0,&ts,NULL)==-1)
+    errExit("timerfd_settime");
+
+  if(clock_gettime(CLOCK_MONOTONIC,&start)==-1)
+    errExit("clock_gettime");
+
+  for(totalExp=0;totalExp<maxExp;){
+    s=read(fd,&numExp,sizeof(uint64_t));
+    if(s!=sizeof(uint64_t))
+      errExit("read");
+
+    totalExp+=numExp;
+    if(clock_gettime(CLOCK_MONOTONIC,&now)==-1)
+      errExit("clock_gettime");
+
+    secs=now.tv_sec-start.tv_sec;
+    nanosecs=now.tv_nsec-start.tv_nsec;
+    if(nanosecs<0){
+      secs--;
+      nanosecs+=1000000000;
+    }
+    printf("%d.%03d:expirations read:%llu;total=%llu\n",
+	   secs,(nanosecs+500000)/1000000,
+	   (unsigned long long)numExp,(unsigned long long)totalExp);
+  }
+  exit(EXIT_SUCCESS);
+}
