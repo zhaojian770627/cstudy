2d6751a6190ef050718262f124473793d2f7b88b
zhaojian <zhaojian770627@163.com>
2016-12-01 21:54:55 +0800

    b

---
+++ linuxmanaual/src/procexec/fork_stdio_buf.c
@@ -0,0 +1,12 @@
+#include "../lib/tlpi_hdr.h"
+
+int main(int argc,char *argv[])
+{
+  printf("Hello world\n");
+  write(STDOUT_FILENO,"Ciao\n",5);
+
+  if(fork()==-1)
+    errExit("fork");
+
+  exit(EXIT_SUCCESS);
+}
