a68a2e635c7f57fcccad3fddf65dbd40ccca5f14
zhaojian <zhaojian770627@163.com>
2016-12-01 20:56:26 +0800

    exit_handlers.c

--- linuxmanaual/src/procexec/exit_handlers.c
+++ linuxmanaual/src/procexec/exit_handlers.c
@@ -19,5 +19,13 @@ static void onexitFunc(int exitStatus,void *arg)
 
 int main(int argc,char *argv[])
 {
-  exit(EXIT_SUCCESS);
+  if(on_exit(onexitFunc,(void*)10)!=0)
+    fatal("on_exit 1");
+  if(atexit(atexitFunc1)!=0)
+    fatal("atexit 1");
+  if(atexit(atexitFunc2)!=0)
+    fatal("atexit 2");
+  if(on_exit(onexitFunc,(void*)20)!=0)
+    fatal("on_exit 2");
+  exit(2);
 }
