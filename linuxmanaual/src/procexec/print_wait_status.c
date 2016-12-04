a292de00740cbe119324c4e079dd83c000d560a8
zhaojian <zhaojian770627@163.com>
2016-12-04 17:33:27 +0800

    a

---
+++ linuxmanaual/src/procexec/print_wait_status.c
@@ -0,0 +1,23 @@
+#define _GNU_SOURCE
+#include <string.h>
+#include <sys/wait>
+#include "print_wait_status.h"
+#include "../lib/tlpi_hdr.h"
+
+void printWaitStatus(cont char *msg,int status)
+{
+  if(msg!=NULL)
+    printf("%s",msg);
+  if(WIFEXITED(status)){
+    printf("child exited,status=%d\n",WEXITSTATUS(status));
+  }
+  else if(WIFSIGNALED(status)){
+    printf("child killed by signal %d (%s)",
+	   WTERMSIG(status),strsignal(WTERMSIG(status)));
+    #ifdef WCOREDUMP
+    if(WCOREDUMP(status))
+      printf("(core dumped)");
+    #endif
+    printf("\n");
+  }else if(WIFSTOPPED(status)){
+    printf("child stopped by signal %d (%s)\n",
