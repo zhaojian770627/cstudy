eee9f2717ca8a7abb7f6b812f3fce38327b8cb9f
zhaojian770627 <zhaojian770627@163.com>
2017-01-11 13:54:45 +0800

    up

--- linuxmanaual/src/cap/check_password_caps.c
+++ linuxmanaual/src/cap/check_password_caps.c
@@ -1,1 +1,17 @@
 #define _BSD_SOURCE 		/* Get getpass() declareation from <unsistd.h */
+#define _XOPEN_SOURCE		/* Get crypt() declaration from <unistd.h */
+#include <sys/capability.h>
+#include <unistd.h>
+#include <limits.h>
+#include <pwd.h>
+#include <shadow.h>
+#include "../lib/tlpi_hdr.h"
+
+/* Change setting of capability in caller's effective capabilities */
+static int modifyCap(int capability,int setting)
+{
+  cap_t caps;
+  cap_value_t capList[1];
+
+  caps=cap_get_proc();
+}
