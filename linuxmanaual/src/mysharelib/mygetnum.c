4a9b725b65630eeea535a768259d84c055f9f840
zhaojian770627 <zhaojian770627@163.com>
2017-01-15 17:37:49 +0800

    add

---
+++ linuxmanaual/src/mysharelib/mygetnum.c
@@ -0,0 +1,13 @@
+/*
+ * get_num.c
+ *
+ * Functions to process numeric command-line arguments
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+int getInt(const char *arg, int flags, const char *name) {
+  return 1234;
+}
