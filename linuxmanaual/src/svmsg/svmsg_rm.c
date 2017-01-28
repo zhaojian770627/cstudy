329352c3b3f1548dfd08a1f56808b05b3ac04834
zhaojian770627 <zhaojian770627@163.com>
2017-01-28 19:07:40 +0800

    add

---
+++ linuxmanaual/src/svmsg/svmsg_rm.c
@@ -0,0 +1,16 @@
+#include <sys/types.h>
+#include <sys/msg.h>
+#include "../lib/tlpi_hdr.h"
+
+int main(int argc,char *argv[])
+{
+  int j;
+  if(argc>1 && strcmp(argv[1],"--help")==0)
+    usageErr("%s [msqid...]\n",argv[0]);
+
+  for(j=1;j<argc;j++)
+    if(msgctl(getInt(argv[j],0,"msqid"),IPC_RMID,NULL)==-1)
+      errExit("msgctl %s",argv[j]);
+
+  exit(EXIT_SUCCESS);
+}
