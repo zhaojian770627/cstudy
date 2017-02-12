b75ec36cd3c6bb0e6be81cc0ad69f154c07533d9
zhaojian770627 <zhaojian770627@163.com>
2017-02-12 17:19:26 +0800

    add

---
+++ linuxmanaual/src/mmap/t_mmap.c
@@ -0,0 +1,42 @@
+#include <sys/mman.h>
+#include <fcntl.h>
+#include "../lib/tlpi_hdr.h"
+
+#define MEM_SIZE 10
+
+int main(int argc,char *argv[])
+{
+  char *addr;
+  int fd;
+  struct stat sb;
+
+  if(argc<2||strcmp(argv[1],"--help")==0)
+    usageErr("%s file[new-value]\n",argv[0]);
+
+  fd=open(argv[1],O_RDWR);
+  if(fd==-1)
+    errExit("open");
+
+  addr=mmap(NULL,MEM_SIZE,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+  if(addr==MAP_FAILED)
+    errExit("mmap");
+
+  if(close(fd)==-1)		/* No longer need 'fd' */
+    errExit("close");
+
+  printf("Current string=%.*s\n",MEM_SIZE,addr);
+
+  /* Update contents of region */
+  if(argc>2){
+    if(strlen(argv[2])>=MEM_SIZE)
+      cmdLineErr("'new-value' too large\n");
+
+    memset(addr,0,MEM_SIZE);	/* Zero out region */
+    strncpy(addr,argv[2],MEM_SIZE-1);
+    if(msync(addr,MEM_SIZE,MS_SYNC)==-1)
+      errExit("msync");
+    printf("Copied \"%s\" to shared memory\n",argv[2]);
+  }
+
+  exit(EXIT_SUCCESS);
+}
