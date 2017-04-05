d4efef717f2957b3c0c2080edba1e4615e39dd17
zhaojian770627 <zhaojian770627@163.com>
2017-04-05 22:35:57 +0800

    add

--- bcyy/crowbar01/eval.c
+++ bcyy/crowbar01/eval.c
@@ -35,3 +35,200 @@ eval_double_expression(double double_value)
 
   return v;
 }
+
+static CRB_Value
+eval_string_expression(CRB_Interpreter *inter,char *string_value)
+{
+  CRB_Value v;
+
+  v.type=CRB_STRING_VALUE;
+  v.u.string_value=crb_literal_to_crb_string(inter,string_value);
+
+  return v;
+}
+
+static CRB_Value
+eval_null_expression(void)
+{
+  CRB_Value v;
+
+  v.type=CRB_NULL_VALUE;
+
+  return v;
+}
+
+static void
+refer_if_string(CRB_Value *v)
+{
+  if(v->type==CRB_STRING_VALUE){
+    crb_release_string(v->u.string_value);
+  }
+}
+
+static Variable *
+search_global_variable_from_env(CRB_Interpreter *inter,
+				LocalEnvironment *env,char *name)
+{
+  GlobalVariableRef *pos;
+
+  if(env==NULL){
+    return crb_search_global_variable(inter,name);
+  }
+
+  for(pos=env->global_variable;pos;pos=pos->next){
+    if(!strcmp(pos->variable->name,name)){
+      return pos->variable;
+    }
+  }
+
+  return NULL;
+}
+
+static CRB_Value
+eval_identifier_expression(CRB_Interpreter *inter,
+			   LocalEnvironment *env,Expression *expr)
+{
+  CRB_Value v;
+  Variable *vp;
+
+  vp=crb_search_local_variable(env,expr->u.identifier);
+  if(vp!=NULL){
+    v=vp->value;
+  }else{
+    vp=search_global_variable_from_env(inter,env,
+				       expr->u.identifier);
+    if(vp!=NULL){
+      v=vp->value;
+    }else{
+      crb_runtime_error(expr->line_number,VARIABLE_NOT_FOUND_ERR<
+			STRING_MESSAGE_ARGUMENT,
+			"name",expr->u.identifier,
+			MESSAGE_ARGUMENT_END);
+    }
+  }
+  refer_if_string(&v);
+  return v;
+}
+
+static CRB_Value_eval_expression(CRB_Interpreter *inter,
+				 LocalEnvironment *env,
+				 Exprssion *expr);
+
+static CRB_Value 
+eval_assign_expression(CRB_Interpreter *inter,LocalEnvironment *env,
+		       char *identifier,Expression *expression)
+{
+  CRB_Value v;
+  Variable *left;
+
+  v=eval_expression(inter,env,expression);
+
+  left=crb_search_local_variable(env,identifier);
+  if(left==NULL){
+    left=search_global_variable_from_env(inter,env,identifier);
+  }
+  if(left!=NULL){
+    left=search_global_variable_from_env(inter,env,identifier);
+  }
+  if(left!=NULL){
+    release_if_string(&left->value);
+    left->value=v;
+    refer_if_string(&v);
+  }else{
+    if(env!=NULL){
+      crb_add_local_variable(env,identifier,&v);
+    }else{
+      CRB_add_global_variable(inter,identifier,&v);
+    }
+    refer_if_string(&v);
+  }
+
+  return v;
+}
+
+static CRB_Boolean
+eval_binary_boolean(CRB_Interpreter *inter,ExpressionType operator,
+		    CRB_Boolean left,CRB_Blloean right,int line_number)
+{
+  CRB_Boolean result;
+
+  if(operator==EQ_EXPRESSION){
+    result=left==right;
+  }else if(operator==NE_EXPRESSION){
+    result=left!=right;
+  }else{
+    char *op_str=crb_get_operator_string(operator);
+    crb_runtime_error(line_number,NOT_BOOLEAN_OPERATOR_ERR,
+		      STRING_MESSAGE_ARGUMENT,"operator",op_str,
+		      MESSAGE_ARGUMENT_END);
+  }
+  return result;
+}
+
+static void
+eval_binary_int(CRB_Interpreter *inter,ExpressionType operator,
+		int left,int right,
+		CRB_Value *result,int line_number)
+{
+  if(dkc_is_math_operator(operator)){
+    result->type=CRB_INT_VALUE;
+  }else if(dkc_is_compare_operator(operator)){
+    result->type=CRB_BOOLEAN_VALUE;
+  }else{
+    DBG_panic(("operator..%d\n",operator));
+  }
+
+  switch(operator){
+  case BOOLEAN_EXPRESSION:	/* FALLTHRU */
+  case INT_EXPRESSION:		/* FALLTHRU */
+  case DOUBLE_EXPRESSION:	/* FALLTHRU */
+  case STRING_EXPRESSION:	/* FALLTHRU */
+  case IDENTIFIER_EXPRESSION:	/* FALLTHRU */
+  case ASSIGN_EXPRESSION:
+    DBG_panic(("bad case...%d",operator));
+    break;
+  case ADD_EXPRESSION:
+    result->u.int_value=left+right;
+    break;
+  case SUB_EXPRESSION:
+    result->u.int_value=left-right;
+    break;
+  case MUL_EXPRESSION:
+    result->u.int_value=left*right;
+    break;
+  case DIV_EXPRESSION:
+    result->u.int_value=left/right;
+    break;
+  case MOD_EXPRESSION:
+    result->u.int_value=left%right;
+    break;
+  case LOGICAL_AND_EXPRESSION:	/* FALLTHRU */
+  case LOGICAL_OR_EXPRESSION:
+    DBG_panic("bad case...%d",operator);
+    break;
+  case EQ_EXPRESSION:
+    result->u.boolean_value=left==right;
+    break;
+  case NE_EXPRESSION:
+    result->u.boolean_value=left!=right;
+    break;
+  case GT_EXPRESSION:
+    result->u.boolean_value=left>right;
+    break;
+  case GE_EXPRESSION:
+    result->u.boolean_value=left>=right;
+    break;
+  case LT_EXPRESSION:
+    result->u.boolean_value=left<right;
+    break;
+  case LE_EXPRESSION:
+    result->u.boolean_value=left<=right;
+    break;
+  case MINUS_EXPRESSION:	/* FALLTHRU */
+  case FUNCTION_CALL_EXPRESSION: /* FALLTHRU */
+  case NULL_EXPRESSION:		 /* FALLTHRU */
+  case EXPRESSION_TYPE_COUNT_PLUS_1: /* FALLTHRU */
+  default:
+    DBG_panic(("bad case..%d",operator));
+  }
+}
