ef1c5cdf82428a0e6bab97328fb841aba16ccdf9
zhaojian770627 <zhaojian770627@163.com>
2017-04-11 23:18:48 +0800

    add

--- oranges/3/pmtest2.s
+++ oranges/3/pmtest2.s
@@ -19,10 +19,38 @@ LABEL_DESC_VIDEO:	Descriptor	0B8000h,	0ffffh,		DA_DRW
 	GdtPtr	dw	GdtLen-1    ;GDT Limit
 		dd	0
 	;; GDT selector
+	SelectorNormal	equ	LABEL_DESC_NORMAL - LABEL_GDT
 	SelectorCode32	equ	LABEL_DESC_CODE32 - LABEL_GDT
+	SelectorCode16	equ	LABEL_DESC_CODE16 - LABEL_GDT
+	SelectorData	equ	LABEL_DESC_DATA	- LABEL_GDT
+	SelectorStack	equ	LABEL_DESC_STACK - LABEL_GDT
+	SelectorTest 	equ 	LABEL_DESC_TEST - LABEL_GDT
 	SelectorVideo	equ	LABEL_DESC_VIDEO - LABEL_GDT
 	;; End of [Section .gdt]
 
+	[SECTION .data1]	;data section
+	ALIGN 32
+	[BITS 32]
+LABEL_DATA:
+	SPValueInRealMode	dw	0
+	;; string
+PMMessage:	db	"In Protect Mode now.^-^",0 ;diplay in protected
+	OffsetPMMessage	equ	PMMessage - $$
+StrTest:	db	"ABCDEFGHIJKLMNOPQRSTUVWXYZ",0
+	OffsetStrTest	equ	StrTest - $$
+	DataLen	equ	$ - LABEL_DATA
+	;; End of [SECTION .data]
+
+	;; global stack sector
+	[SECTION .gs]
+	ALIGN	32
+	[BITS	32]
+LABEL_STACK:
+	times 	512 db	0
+
+	TopOfStack	equ	$ - LABEL_STACK -1
+	;; End of [SECTION .gs]
+	
 	[SECTION .s16]
 	[BITS 16]
 LABEL_BEGIN:
@@ -72,13 +100,39 @@ LABEL_BEGIN:
 	[SECTION .s32]		;32bit code
 	[BITS	32]
 LABEL_SEG_CODE32:
+	mov	ax,SelectorData
+	mov	ds,ax		;Data selector
+	mov	ax,SelectorTest
+	mov	es,ax
 	mov 	ax,SelectorVideo
 	mov	gs,ax
-	mov	edi,(80*11+79)*2 ;screen 11 row 79 col
-	mov	ah,0ch		 ;red on black
-	mov 	al,'P'
-	mov	[gs:edi],ax
 
+	mov	ax,SelectorStack
+	mov	ss,ax		 ;Stack section selector
+
+	mov	esp,TopOfStack
+
+	;; Display a string
+	
+	mov	edi,(80*11+79)*2 ;screen 11 row 79 col
+
+	mov	ah,0ch		 ;red on black
+	xor	esi,esi
+	xor	edi,edi
+	mov	esi,OffsetPMMessage ;source
+	mov	edi,(80*10+0)*2	    ;
+	cld
+
+.1:
+	lodsb
+	test	al,al
+	jz	.2
+	mov	[gs:edi],ax
+	add	edi,2
+	jmp	.1
+
+.2:	// Here
+	
 	jmp	$
 	SegCode32Len	equ	$-LABEL_SEG_CODE32
 	;; END of [SECTION .32
