6f82ce0b93576d6af6d9cf476049b42fd540bf79
zhaojian770627 <zhaojian770627@163.com>
2017-04-13 23:04:31 +0800

    up

--- oranges/3/pmtest2.s
+++ oranges/3/pmtest2.s
@@ -60,6 +60,19 @@ LABEL_BEGIN:
 	mov 	ss,ax
 	mov 	sp,0100h
 
+	mov	[LABEL_GO_BACK_TO_REAL+3],ax
+	mov	[SPValueInRealMode],sp
+
+	;; Init 16bit Code desc
+	mav	ax,cs
+	movzx	eax,ax
+	shl	eax,4
+	add	eax,LABEL_SEG_CODE16
+	mov	word[LABEL_DESC_CODE16+2],ax
+	shr	eax,16
+	mov	byte[LABEL_DESC_CODE16+4],al
+	mov	byte[LABEL_DESC_CODE16+7],ah
+	
 	;; Init 32bit section descriptor
 	xor	eax,eax
 	mov 	ax,cs
@@ -70,6 +83,27 @@ LABEL_BEGIN:
 	mov	byte[LABEL_DESC_CODE32+4],al
 	mov	byte[LABEL_DESC_CODE32+7],ah
 
+	;; Init Data section descriptor
+	xor	eax,eax
+	mov	ax,ds
+	shl	eax,4
+	add	eax,LABEL_DATA
+	mov	word[LABEL_DATA+2],ax
+	shr	eax,16
+	mov	byte[LABEL_DATA+4],al
+	mov	byte[LABEL_DATA+7],ah
+
+	;; Init stack section descriptor
+	xor 	eax,eax
+	mov	ax,ds
+	sh	eax,4
+	add	eax,LABEL_STACK
+	mov	word[LABEL_DESC_STACK+2],ax
+	shr	eax,16
+	mov	byte[LABEL_DESC_STACK+4],al
+	mov	byte[LABEL_DESC_STACK+7],ah
+	
+	
 	;; prepare load GDTR
 	xor	eax,eax
 	mov	ax,ds
@@ -97,6 +131,22 @@ LABEL_BEGIN:
 	jmp	dword SelectorCode32:0
 	;; End of [SECTION .s16]
 
+LABEL_REAL_ENTRY:
+	mov	ax,cs
+	mov	ds,ax
+	mov	es,ax
+	mov	ss,ax
+
+	mov	sp,[SPValueInRealMode]
+
+	int 	al,92h
+	and 	al,11111101b
+	out	92h,al
+	sti
+
+	mov	ax,4200
+	int 	21h
+	
 	[SECTION .s32]		;32bit code
 	[BITS	32]
 LABEL_SEG_CODE32:
@@ -114,8 +164,6 @@ LABEL_SEG_CODE32:
 
 	;; Display a string
 	
-	mov	edi,(80*11+79)*2 ;screen 11 row 79 col
-
 	mov	ah,0ch		 ;red on black
 	xor	esi,esi
 	xor	edi,edi
@@ -131,8 +179,124 @@ LABEL_SEG_CODE32:
 	add	edi,2
 	jmp	.1
 
-.2:	// Here
-	
+	 
+.2:	  			;;DIsplay over
+	call 	DispReturn
+
+	call	TestRead
+	call	TestWrite
+	call	TestRead
+
+	;; Stop
+	jmp	SelectorCode16:0
+
+TestRead:
+	xor	esi,esi
+	mov	ecx,8
+.loop:
+	mov	al,[es.esi]
+	call	 DispAL
+	inc	esi
+	loop	.loop
+
+	call	DispReturn
+
+	ret
+	;; TestRead end
+
+TestWrite:
+	push 	esi
+	push 	edi
+	xor	esi,esi
+	xor	edi,edi
+	mov	esi,OffsetStrTest ;soruce data offset
+	cld
+.1:
+	lodsb
+	test	al,al
+	jz	.2
+	mov	[es:edi],al
+	inc	edi
+	jmp	.1
+.2:
+	pop	edi
+	pop	esi
+
+	ret
+	;; TestWrite End
+
+	;; --------------------------------------
+	;; Display Number in AL
+	;; default number has already in AL
+	;; edi point next char pos
+	;; register:ax,edi
+DispAL:
+	push	ecx
+	push	edx
+
+	mov	ah,0ch		;0000 bg black 1100 fg red
+	mov	dl,al
+	shr	al,4
+	mov	ecx,2
+.begin:
+	and	al,01111b
+	cmp	al,9
+	ja	.1
+	add	al,'0'
+	jmp	.2
+.1:
+	sub	al,0ah
+	add	al,'A'
+.2:
+	mov	[gs:edi',ax
+	add	edi,2
+
+	mov	al,dl
+	loop	.begin
+	add	edi,2
+
+	pop	edx
+	pop 	ecx
+	ret
+	;; DespAL end
+
+	;; ---------------------------------------
+DispReturn:
+	push 	eax
+	push	ebx
+	mov	eax,edi
+	mov	bl,160
+	div	bl
+	and 	eax,0ffh
+	inc	eax
+	mov	bl,160
+	mul	bl
+	mov	edi,eax
+	pop 	ebx
+	pop	eax
+
+	ret
+	;; DispReturn End
 	jmp	$
 	SegCode32Len	equ	$-LABEL_SEG_CODE32
 	;; END of [SECTION .32
+
+	[SECTION .s16code]
+	ALIGN 32
+	[BITS 16]
+	mov	ax,SelectorNormal
+	mov	ds,ax
+	mov	es,ax
+	mov	fs,ax
+	mov	gs,ax
+	mov	ss,ax
+
+	mov	eax,cr0
+	and 	al,11111110b
+	mov	cr0,eax
+
+LABEL_GO_BACK_TO_REAL:
+	jmp	0:LABEL_REAL_ENTRY
+
+	Code16Len	equ $-LABEL_SEG_CODE16
+	;; End of [SECTION .s16code]
