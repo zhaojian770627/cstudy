1278690866111b31f48c45087f2e1b3a73d8da14
zhaojian770627 <zhaojian770627@163.com>
2017-04-20 22:24:36 +0800

    up

--- bcyy/crowbar01/MEM.h
+++ bcyy/crowbar01/MEM.h
@@ -74,8 +74,8 @@ void MEM_check_all_blocks_func(MEM_Controller controller,
 		   __FILE__,__LINE__,str))
 
 #define MEM_open_storage(page_size)\
-  (MEM_storage_malloc_func(MEM_CURRENT_CONTROLLER,\
-			   __FILE__,__LINE__,storage,size))
+  (MEM_open_storage_func(MEM_CURRENT_CONTROLLER,\
+			   __FILE__,__LINE__,page_size))
 
 #define MEM_storage_malloc(storage,size)			\
   (MEM_storage_malloc_func(MEM_CURRENT_CONTROLLER,		\
