ffe1c254e79305dfc1b2915f74f9eaa328e712cf
zhaojian770627 <zhaojian770627@163.com>
2017-04-25 22:59:30 +0800

    add

--- linuxmanaual/src/altio/t_select.c
+++ linuxmanaual/src/altio/t_select.c
@@ -13,3 +13,63 @@ usageError(const char *progName)
   exit(EXIT_FAILURE);
 }
 
+int
+main(int argc,char *argv[])
+{
+  fd_set readfds,writefds;
+  int ready,nfds,fd,numRead,j;
+  struct timeval timeout;
+  char buf[10];			/* Large enouth to hold "rw\0" */
+
+  if(argc<2 || strcmp(argv[1],"--help")==0)
+    usageError(argv[0]);
+
+  /* Timeout for select() is specified in argv[1] */
+  if(strcmp(argv[1],"-")==0){
+      pto=NULL;			/* Infinite timeout */
+  }else{
+    pto=&timeout;
+    timeout.tv_sec=getLong(argv[1],0,"timeout");
+    timeout.tv_usec=0;		/* No microseconds */
+  }
+
+  /* Process remaining arguments to build file descriptor sets */
+  nfds=0;
+  FD_ZERO(&readfds);
+  FD_ZERO(&writefds);
+
+  for(j=2;j<argc;j++){
+    numRead=sscanf(argv[j],"%d%2[rw]",&fd,buf);
+    if(numRead!=2)
+      usageError(argv[0]);
+    if(fd>=FD_SETSIZE)
+      cmdLineErr("file descriptor exceeds limit (%d)\n",FD_SETSIZE);
+
+    if(fd>=nfds)
+      nfds=fd+1;		/* Record maximum fd+1 */
+
+    if(strchr(buf,'r')!=NULL)
+      FD_SET(fd,&readfds);
+
+    if(strchr(buf,'w')!=NULL)
+      FD_SET(fd,&writefds);
+  }
+
+  /* We've built all of the arguments;now call select() */
+  ready=select(nfds,&readfds,&writefds,NULL,pto);
+  /* Ignore exceptional events */
+
+  if(ready==-1)
+    errExit("select");
+
+  /* Display results of select() */
+  printf("read=%d\n",ready);
+  for(fd=0;fd<nfds;fd++)
+    printf("%d:%s%s\n",fd,FD_ISSET(fd,&readfds)?"r":"",
+	   FD_ISSET(fd,&writefds)?"w":"");
+
+  if(pto!=NULL)
+    printf("timeout after select():%ld.%03ld\n",
+	   (long)timeout.tv_sec,(long)timeout.tv_usec/10000);
+  exit(EXIT_SUCCESS);
+}
