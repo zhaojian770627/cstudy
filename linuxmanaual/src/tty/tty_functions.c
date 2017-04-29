24b79a1149403c640165ffec158c2ab6d78a1c8a
zhaojian770627 <zhaojian770627@163.com>
2017-04-29 21:21:36 +0800

    ad

---
+++ linuxmanaual/src/tty/tty_functions.c
@@ -0,0 +1,59 @@
+#include <termios.h>
+#include <unistd.h>
+#include "tty_functions.h"
+
+int
+ttySetCbreak(int fd,struct termios *prevTermios)
+{
+  struct termios t;
+
+  if(tcgetattr(fd,&t)==-1)
+    return -1;
+
+  if(prevTermios!=NULL)
+    *prevTermios=t;
+
+  t.c_lflag&=~(ICANON|ECHO);
+  t.c_lflag|=~ICRNL;
+
+  t.c_lflag&=~ICRNL;
+  t.c_cc[VMIN]=1;		/* Character-at-a-time input */
+  t.c_cc[VTIME]=0;		/* with blocking */
+
+  if(tcsetattr(fd,TCSAFLUSH,&t)==-1)
+    return -1;
+
+  return 0;
+}
+
+int
+ttySetRaw(int fd,struct termios *prevTermios)
+{
+  struct termios t;
+
+  if(tcgetattr(fd,&t)==-1)
+    return -1;
+
+  if(prevTermios!=NULL)
+    *prevTermios=t;
+
+  /* Noncanonical mode,disable signals,extended
+     input processing,and echoing */
+  t.c_iflag &=~(ICANON|ISIG|IEXTEN|ECHO);
+
+  /* Disable special handing of CR,NL,and BREAK.
+   No 8th-bit stripping or parity error handling.
+   Disable START/STOP output flow control.*/
+  t.c_iflag&=~(BRKINT|ICRNL|IGNBRK|IGNCR|INLCR|
+	       INPCK|ISTRIP|IXON|PARMRK);
+
+  t.c_oflag&=~OPOST;		/* Disable all output processing */
+
+  t.c_cc[VMIN]=-1;		/* Character-at-a-time input */
+  t.c_cc[VTIME]=0;		/* with blocking */
+
+  if(tcsetattr(fd,TCSAFLUSH,&t)==-1)
+    return -1;
+
+  return 0;
+}
