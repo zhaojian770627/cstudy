f9c3a5c5543c6386551ee4b1793d27b5b9bffc63
zhaojianc <zhaojianc@yonyou.com>
2017-11-04 15:29:12 +0800

    add

--- bcyy/crowbar02/heap.c
+++ bcyy/crowbar02/heap.c
@@ -47,3 +47,103 @@ add_ref_in_native_method(CRB_LocalEnvironment *env,CRB_Object *obj)
   new_ref->next=env->ref_in_native_method;
   env->ref_in_native_method=new_ref;
 }
+
+CRB_Object *
+crb_literal_to_crb_string(CRB_Interpreter *inter,char *str)
+{
+  CRB_Object *ret;
+
+  ret=alloc_object(inter,STRING_OBJECT);
+  ret->u.string.string=str;
+  ret->u.string.is_literal=CRB_TRUE;
+
+  return ret;
+}
+
+CRB_Object *
+crb_create_crowbar_string_i(CRB_Interpreter *inter,char *str)
+{
+  CRB_Object *str;
+
+  ret=alloc_object(inter,STRING_OBJECT);
+  ret->u.string.string=str;
+  inter->heap.current_heap_size+=strlen(str)+1;
+  ret->u.string.is_literal=CRB_FALSE;
+
+  return ret;
+}
+
+CRB_Object *
+CRB_create_crowbar_string(CRB_Interpreter *inter,CRB_LocalEnvironment *env,
+			  char *str)
+{
+  CRB_Object *ret;
+  ret=crb_create_crowbar_string_i(inter,str);
+  add_ref_in_native_method(env,ret);
+
+  return ret;
+}
+
+CRB_Object *
+crb_create_array_i(CRB_Interpreter *inter,int size)
+{
+  CRB_Object *ret;
+
+  ret=alloc_object(inter,ARRAY_OBJECT);
+  ret->u.array.size=size;
+  ret->u.array.alloc_size=size;
+  ret->u.array.array=MEM_malloc(sizeof(CRB_Value)*size);
+  inter->heap.current_heap_size+=sizeof(CRB_Value)*size;
+
+  return ret;
+}
+
+CRB_Object *
+CRB_create_array(CRB_Interpreter *inter,CRB_Localenvironment *env,
+		 int size)
+{
+  CRB_Object *ret;
+
+  ret=crb_create_array_i(inter,size);
+  add_ref_in_native_method(env,ret);
+
+  return ret;
+}
+
+void
+crb_array_add(CRB_Interpreter *inter,CRB_Object *obj,CRB_Value v)
+{
+  int new_size;
+
+  DBG_assert(obj->type==ARRAY_OBJECT,("bad type..%d\n",obj->type));
+
+  check_gc(inter);
+  if(obj->u.array.size+1>obj->u.array.alloc_size){
+    new_size=obj->u.array.alloc_size*2;
+    if(new_size==0
+       ||new_size-obj->u.array.alloc_size>ARRAY_ALLOC_SIZE){
+      new_size=obj->u.array.alloc_size+ARRAY_ALLOC_SIZE;
+    }
+    obj->u.array.array=MEM_realloc(obj->u.array.array,
+				   new_size*sizeof(CRB_Value));
+    inter->heap.current_heap_size
+      +=(new_size-obj->u.array.alloc_size)*sizeof(CRB_Value);
+    obj->u.array.alloc_size=new_size;
+  }
+  obj->u.array.array[obj->u.array.size]=v;
+  obj->u.array.size++;
+}
+
+void
+crb_array_resize(CRB_Interpreter *inter,CRB_Object *obj,int new_size)
+{
+  int new_alloc_size;
+  CRB_Boolean need_realloc;
+  int i;
+
+  check_gc(intter);
+
+  if(new_size>obj->u.array.alloc_size){
+    new_alloc_size=obj->u.array.alloc_size*2;
+    if(new_alloc_size<new_size){
+      new_alloc_size=new_size+ARRAY_ALLOC_SIZE;
