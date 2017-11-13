1c1d3a26bfa67d8419bd71e646e3d371986c68b1
zhaojianc <zhaojianc@yonyou.com>
2017-11-13 16:50:21 +0800

    add

--- linuxmanaual/src/mytools/testsize.c
+++ linuxmanaual/src/mytools/testsize.c
@@ -11,6 +11,10 @@
 #define CHARMAX 1024
 
 int main(int argc, char *argv[]) {
-  printf("Elf32_Half size  %ld\n",sizeof(Elf32_Half));
+  printf("Elf64_Addr size: %ld\n",sizeof(Elf64_Addr));
+  printf("Elf64_Half size: %ld\n",sizeof(Elf64_Half));
+  printf("Elf64_Off size: %ld\n",sizeof(Elf64_Off));
+  printf("Elf64_Sword size: %ld\n",sizeof(Elf64_Sword));
+  printf("Elf64_Word size: %ld\n",sizeof(Elf64_Word));
   exit(EXIT_SUCCESS);
 }
