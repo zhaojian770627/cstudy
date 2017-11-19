a73d9aacea7c3f9a56d4851a75f10a34adb675c9
zhaojian770627 <zhaojian770627@163.com>
2017-11-19 20:01:36 +0800

    add

--- oranges/5/loader.s
+++ oranges/5/loader.s
@@ -1,12 +1,29 @@
 	org	0100h		;调试状态，做成.COM文件，可调试
-BaseOfStack		equ	0100h	; 调试状态下堆栈基地址(栈底, 从这个位置向低地址生长)
-
-BaseOfKernelFile	equ	08000h	;KERNEL.BIN　被加载到的位置---段地址
-OffsetOfKernelFile	equ	0h	;KERNEL.BIN　被加载到的位置---偏移地址
 
 	jmp	short LABEL_START ;Start to boot.
 
 %include "fat12hdr.inc"
+%include "load.inc"
+%include "pm.inc"
+
+; GDT
+;                            段基址     段界限, 属性
+LABEL_GDT:	    Descriptor 0,            0, 0              ; 空描述符
+LABEL_DESC_FLAT_C:  Descriptor 0,      0fffffh, DA_CR|DA_32|DA_LIMIT_4K ;0-4G
+LABEL_DESC_FLAT_RW: Descriptor 0,      0fffffh, DA_DRW|DA_32|DA_LIMIT_4K;0-4G
+LABEL_DESC_VIDEO:   Descriptor 0B8000h, 0ffffh, DA_DRW|DA_DPL3 ; 显存首地址
+
+GdtLen		equ	$ - LABEL_GDT
+GdtPtr		dw	GdtLen - 1				; 段界限
+		dd	BaseOfLoaderPhyAddr + LABEL_GDT		; 基地址
+
+; GDT 选择子
+SelectorFlatC		equ	LABEL_DESC_FLAT_C	- LABEL_GDT
+SelectorFlatRW		equ	LABEL_DESC_FLAT_RW	- LABEL_GDT
+SelectorVideo		equ	LABEL_DESC_VIDEO	- LABEL_GDT + SA_RPL3	
+BaseOfStack		equ	0100h	; 调试状态下堆栈基地址(栈底, 从这个位置向低地址生长)
+PageDirBase		equ	100000h ;页目录开始地址:1M
+PageTblBase		equ	101000h ;页表开始地址:1M+4K
 
 LABEL_START:			;从这里开始
 	mov	ax,cs
@@ -18,6 +35,24 @@ LABEL_START:			;从这里开始
 	mov	dh,0		;"Loading "
 	call	Dispstr		;显示字符串
 
+	;; 得到内存数
+	mov	ebx,0		;ebx=后续值，开始时需为0
+	mov	di,_MemChkBuf	;es:di 指向一个地址范围描述符结构
+.MemChkLoop:
+	mov	eax,0e820h
+	mov	ecx,20
+	mov	edx,0534d4150h
+	int	15h
+	jc	.MemChkFail
+	add	di,20
+	inc	dword[_dwMCRNumber] ;dwMCRNumber=ARDS的个数
+	cmp	ebx,0
+	jne	.MemChkLoop
+	jmp	.MemChkOk
+.MemChkFail:
+	mov	dword[_dwMCRNumber],0
+.MemChkOK:
+	
 	;; 下面在A盘的根目录寻找KERNEL.BIN
 	mov	word[wSectorNo],SectorNoOfRootDirectory
 		
@@ -124,12 +159,28 @@ LABEL_FILE_LOADED:
 	CALL 	KillMotor 	;关闭软驱马达
 
 	mov	dh,1		;"Ready."
-	call	Dispstr
-;;; **************************************************************
-	jmp	$
-;;; ***************************************************
-	
-;;;===========================================================
+	call	DispStrRelMode
+
+	;; 下面准备跳入保护模式
+	;; 加载GDTR
+	lgdt	[GdtPtr]
+
+	;; 关中断
+	cli
+
+	;; 打开地址线A20
+	in	al,92h
+	or	al,00000010b
+	out	92h,al
+
+	;; 准备切换到保护模式
+	mov	eax,cr0
+	or	eax,1
+	mov	cr0,eax
+
+	;; 真正进入保护模式
+	jmp	dword	SelectorFlatC:(BaseOfLoaderPhyAddr+LABEL_PM_START)
+;;;==========================================================================
 ;;; 变量
 ;Root　Directory占用的扇区数,在循环中会递减至0
 wRootDirSizeForLoop	dw	RootDirSectors 
@@ -146,7 +197,12 @@ Message1		db	"Ready.   " ; 9字节, 不够则用空格补齐. 序号 1
 Message2		db	"No KERNEL" ; 9字节, 不够则用空格补齐. 序号 2
 Message3		db	"FOUND    " ; 9字节, 不够则用空格补齐. 序号 3
 ;;; ============================================================
-Dispstr:
+
+;;; -----------------------------------------------------
+;;; DispStrRelMode
+;;; 	显示一个字符串，函数开始时dh中应该是字符串序号(0-based)
+
+DispStrRealMode:
 	mov	ax,MessageLength
 	mul	dh
 	add	ax,LoadMessage
@@ -259,3 +315,176 @@ KillMotor:
 	out	dx,al
 	pop	dx
 	ret
+	
+;;; ======================================================
+;;; 从此以后的代码在保护模式下执行
+;;; 32位代码段，由实模式跳入
+	[SECTION .32]
+	ALIGN	32
+	[BITS 	32]
+LABEL_PM_START:
+	mov	ax,SelectorVideo
+	mov	gs,ax
+
+	mov	ax,SelectorFlatRW
+	mov	ds,ax
+	mov	es,ax
+	mov	fs,ax
+	mov	ss,ax
+	mov	esp,TopOfStack
+
+	push	szMemChkTitle
+	call	dispStr
+	add	esp,4
+
+	call	DispMemInfo
+	call	SetupPaging
+
+	mov	ah,0fh		;0000:黑底 1111:白字
+	mov	al,'P'
+	mov	[ds:((80*0+39)*2)],ax ;屏幕第0行，第39列
+	jmp	$
+
+%include "lib.inc"
+
+;;; 显示内存信息---------------------------------------------
+DispMemInfo:
+	push	esi
+	push	edi
+	push	ecx
+
+	mov	esi, MemChkBuf
+	mov	ecx, [dwMCRNumber];for(int i=0;i<[MCRNumber];i++)//每次得到一个ARDS
+.loop:				  ;{
+	mov	edx, 5		  ;  for(int j=0;j<5;j++)//每次得到一个ARDS中的成员
+	mov	edi, ARDStruct	  ;  {//依次显示:BaseAddrLow,BaseAddrHigh,LengthLow
+.1:				  ;               LengthHigh,Type
+	push	dword [esi]	  ;
+	call	DispInt		  ;    DispInt(MemChkBuf[j*4]); // 显示一个成员
+	pop	eax		  ;
+	stosd			  ;    ARDStruct[j*4] = MemChkBuf[j*4];
+	add	esi, 4		  ;
+	dec	edx		  ;
+	cmp	edx, 0		  ;
+	jnz	.1		  ;  }
+	call	DispReturn	  ;  printf("\n");
+	cmp	dword [dwType], 1 ;  if(Type == AddressRangeMemory)
+	jne	.2		  ;  {
+	mov	eax, [dwBaseAddrLow];
+	add	eax, [dwLengthLow];
+	cmp	eax, [dwMemSize]  ;    if(BaseAddrLow + LengthLow > MemSize)
+	jb	.2		  ;
+	mov	[dwMemSize], eax  ;    MemSize = BaseAddrLow + LengthLow;
+.2:				  ;  }
+	loop	.loop		  ;}
+				  ;
+	call	DispReturn	  ;printf("\n");
+	push	szRAMSize	  ;
+	call	DispStr		  ;printf("RAM size:");
+	add	esp, 4		  ;
+				  ;
+	push	dword [dwMemSize] ;
+	call	DispInt		  ;DispInt(MemSize);
+	add	esp, 4		  ;
+
+	pop	ecx
+	pop	edi
+	pop	esi
+	ret
+
+; 启动分页机制 --------------------------------------------------------------
+SetupPaging:
+	; 根据内存大小计算应初始化多少PDE以及多少页表
+	xor	edx, edx
+	mov	eax, [dwMemSize]
+	mov	ebx, 400000h	; 400000h = 4M = 4096 * 1024, 一个页表对应的内存大小
+	div	ebx
+	mov	ecx, eax	; 此时 ecx 为页表的个数，也即 PDE 应该的个数
+	test	edx, edx
+	jz	.no_remainder
+	inc	ecx		; 如果余数不为 0 就需增加一个页表
+.no_remainder:
+	push	ecx		; 暂存页表个数
+
+	; 为简化处理, 所有线性地址对应相等的物理地址. 并且不考虑内存空洞.
+
+	; 首先初始化页目录
+	mov	ax, SelectorFlatRW
+	mov	es, ax
+	mov	edi, PageDirBase	; 此段首地址为 PageDirBase
+	xor	eax, eax
+	mov	eax, PageTblBase | PG_P  | PG_USU | PG_RWW
+.1:
+	stosd
+	add	eax, 4096		; 为了简化, 所有页表在内存中是连续的.
+	loop	.1
+
+	; 再初始化所有页表
+	pop	eax			; 页表个数
+	mov	ebx, 1024		; 每个页表 1024 个 PTE
+	mul	ebx
+	mov	ecx, eax		; PTE个数 = 页表个数 * 1024
+	mov	edi, PageTblBase	; 此段首地址为 PageTblBase
+	xor	eax, eax
+	mov	eax, PG_P  | PG_USU | PG_RWW
+.2:
+	stosd
+	add	eax, 4096		; 每一页指向 4K 的空间
+	loop	.2
+
+	mov	eax, PageDirBase
+	mov	cr3, eax
+	mov	eax, cr0
+	or	eax, 80000000h
+	mov	cr0, eax
+	jmp	short .3
+.3:
+	nop
+
+	ret
+; 分页机制启动完毕 ----------------------------------------------------------
+
+
+; SECTION .data1 之开始 ---------------------------------------------------------------------------------------------
+[SECTION .data1]
+
+ALIGN	32
+
+LABEL_DATA:
+; 实模式下使用这些符号
+; 字符串
+_szMemChkTitle:	db "BaseAddrL BaseAddrH LengthLow LengthHigh   Type", 0Ah, 0
+_szRAMSize:	db "RAM size:", 0
+_szReturn:	db 0Ah, 0
+;; 变量
+_dwMCRNumber:	dd 0	; Memory Check Result
+_dwDispPos:	dd (80 * 6 + 0) * 2	; 屏幕第 6 行, 第 0 列。
+_dwMemSize:	dd 0
+_ARDStruct:	; Address Range Descriptor Structure
+  _dwBaseAddrLow:		dd	0
+  _dwBaseAddrHigh:		dd	0
+  _dwLengthLow:			dd	0
+  _dwLengthHigh:		dd	0
+  _dwType:			dd	0
+_MemChkBuf:	times	256	db	0
+;
+;; 保护模式下使用这些符号
+szMemChkTitle		equ	BaseOfLoaderPhyAddr + _szMemChkTitle
+szRAMSize		equ	BaseOfLoaderPhyAddr + _szRAMSize
+szReturn		equ	BaseOfLoaderPhyAddr + _szReturn
+dwDispPos		equ	BaseOfLoaderPhyAddr + _dwDispPos
+dwMemSize		equ	BaseOfLoaderPhyAddr + _dwMemSize
+dwMCRNumber		equ	BaseOfLoaderPhyAddr + _dwMCRNumber
+ARDStruct		equ	BaseOfLoaderPhyAddr + _ARDStruct
+	dwBaseAddrLow	equ	BaseOfLoaderPhyAddr + _dwBaseAddrLow
+	dwBaseAddrHigh	equ	BaseOfLoaderPhyAddr + _dwBaseAddrHigh
+	dwLengthLow	equ	BaseOfLoaderPhyAddr + _dwLengthLow
+	dwLengthHigh	equ	BaseOfLoaderPhyAddr + _dwLengthHigh
+	dwType		equ	BaseOfLoaderPhyAddr + _dwType
+MemChkBuf		equ	BaseOfLoaderPhyAddr + _MemChkBuf
+
+
+; 堆栈就在数据段的末尾
+StackSpace:	times	1024	db	0
+TopOfStack	equ	BaseOfLoaderPhyAddr + $	; 栈顶
+; SECTION .data1 之结束 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
