67a73f9cb5ce690fade8fd250d44004b189c4f52
zhaojianc <zhaojianc@yonyou.com>
2017-12-01 13:05:47 +0800

    add

--- linuxmanaual/src/minix/installboot.c
+++ linuxmanaual/src/minix/installboot.c
@@ -7,13 +7,25 @@
 
 #include "../lib/tlpi_hdr.h"
 
-void make_image(char *image, char **procv){
+#define nil 0
 
+void make_image(char *image, char **procv){
+  char *proc,*file;
+  int procn;
+
+  printf("image:%s\n",image);
+  for (procn= 0; (proc= *procv++) != nil; procn++) {
+    if ((file= strchr(proc, ':')) != nil) 
+      file++; 
+    else 
+      file= proc;
+    printf("procv:%s\n",proc);
+  }
 }
 
 int main(int argc, char *argv[]) {
-  char image[50]="";
-  char procv[3][50]={"abc","dddd",0};
+  char image[50]="dddd";
+  char *procv[3]={"abc","dddd",0};
   make_image(image,procv);
   exit(EXIT_SUCCESS);
 }
