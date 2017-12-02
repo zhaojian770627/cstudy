c07292637a321f89fee6ab714e2e884c77665eec
zhaojian770627 <zhaojian770627@163.com>
2017-12-02 22:35:43 +0800

    add

--- oranges/5/kernel.s
+++ oranges/5/kernel.s
@@ -2,12 +2,28 @@
 ; $ nasm -f elf kernel.asm -o kernel.o
 ; $ ld -s kernel.o -o kernel.bin    #‘-s’选项意为“strip all”
 
-[section .text]	; 代码在此
+	SELECTOR_KERNEL_CS	equ	8
+
+	;; 导入函数
+	extern gdt_ptr
+
+	[SECTION .bss]
+	StackSpace	resp	2*1024
+StackTop:			;栈顶
+	[section .text]	; 代码在此
 
 global _start	; 导出 _start
 
-_start:	; 跳到这里来的时候，我们假设 gs 指向显存
-	mov	ah, 0Fh				; 0000: 黑底    1111: 白字
-	mov	al, 'K'
-	mov	[gs:((80 * 1 + 39) * 2)], ax	; 屏幕第 1 行, 第 39 列。
-	jmp	$
+_start:
+	;; 把esp从LOADER挪到KERNEL
+	mov	esp,StackTop	;堆栈在bss段中
+
+	sdgt	[gdt_ptr]	;cstart()中将会用到gdt_ptr
+	call	cstart		;此函数中改变了gdt_ptr,让它指向新的gdt
+	ldgt	[gdt_ptr]	;使用新的GDT
+	;; ldgt	[idt_ptr]
+	jmp	SECTOR_KERNEL_CS:csinit
+csinit:
+	push	0
+	popfd			;Pop top of stack into EFALGS
+	hlt
