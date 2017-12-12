48702ce84b5eb8f8a0c424806329b9ce25429120
zhaojianc <zhaojianc@yonyou.com>
2017-12-12 15:30:44 +0800

    add

--- linuxmanaual/src/minix/testfs.c
+++ linuxmanaual/src/minix/testfs.c
@@ -10,10 +10,122 @@ void showsize()
   printf("super_block size -%d\n",sizeof(struct super_block));
 }
 
+int initsuper(struct super_block *sp){
+  mdev_t dev;
+  int magic;
+  int version, native, r;
+
+  sp->s_dev = NO_DEV;		/* restore later */
+  magic = sp->s_magic;		/* determines file system type */
+
+  /* Get file system version and type. */
+  if (magic == SUPER_MAGIC || magic == conv2(BYTE_SWAP, SUPER_MAGIC)) {
+    version = V1;
+    native  = (magic == SUPER_MAGIC);
+  } else if (magic == SUPER_V2 || magic == conv2(BYTE_SWAP, SUPER_V2)) {
+    version = V2;
+    native  = (magic == SUPER_V2);
+  } else if (magic == SUPER_V3) {
+    version = V3;
+    native = 1;
+  } else {
+    return(EINVAL);
+  }
+
+  /* If the super block has the wrong byte order, swap the fields; the magic
+   * number doesn't need conversion. */
+  sp->s_ninodes =       conv4(native, sp->s_ninodes);
+  sp->s_nzones =        conv2(native, (int) sp->s_nzones);
+  sp->s_imap_blocks =   conv2(native, (int) sp->s_imap_blocks);
+  sp->s_zmap_blocks =   conv2(native, (int) sp->s_zmap_blocks);
+  sp->s_firstdatazone = conv2(native, (int) sp->s_firstdatazone);
+  sp->s_log_zone_size = conv2(native, (int) sp->s_log_zone_size);
+  sp->s_max_size =      conv4(native, sp->s_max_size);
+  sp->s_zones =         conv4(native, sp->s_zones);
+
+  /* In V1, the device size was kept in a short, s_nzones, which limited
+   * devices to 32K zones.  For V2, it was decided to keep the size as a
+   * long.  However, just changing s_nzones to a long would not work, since
+   * then the position of s_magic in the super block would not be the same
+   * in V1 and V2 file systems, and there would be no way to tell whether
+   * a newly mounted file system was V1 or V2.  The solution was to introduce
+   * a new variable, s_zones, and copy the size there.
+   *
+   * Calculate some other numbers that depend on the version here too, to
+   * hide some of the differences.
+   */
+  if (version == V1) {
+    sp->s_block_size = STATIC_BLOCK_SIZE;
+    sp->s_zones = sp->s_nzones;	/* only V1 needs this copy */
+    sp->s_inodes_per_block = V1_INODES_PER_BLOCK;
+    sp->s_ndzones = V1_NR_DZONES;
+    sp->s_nindirs = V1_INDIRECTS;
+  } else {
+    if (version == V2)
+      sp->s_block_size = STATIC_BLOCK_SIZE;
+    if (sp->s_block_size < MIN_BLOCK_SIZE)
+      return EINVAL;
+    sp->s_inodes_per_block = V2_INODES_PER_BLOCK(sp->s_block_size);
+    sp->s_ndzones = V2_NR_DZONES;
+    sp->s_nindirs = V2_INDIRECTS(sp->s_block_size);
+  }
+
+  if (sp->s_block_size < MIN_BLOCK_SIZE) {
+    return EINVAL;
+  }
+  if (sp->s_block_size > MAX_BLOCK_SIZE) {
+    printf("Filesystem block size is %d kB; maximum filesystem\n"
+	   "block size is %d kB. This limit can be increased by recompiling.\n",
+	   sp->s_block_size/1024, MAX_BLOCK_SIZE/1024);
+    return EINVAL;
+  }
+  if ((sp->s_block_size % 512) != 0) {
+    return EINVAL;
+  }
+  if (SUPER_SIZE > sp->s_block_size) {
+    return EINVAL;
+  }
+  if ((sp->s_block_size % V2_INODE_SIZE) != 0 ||
+      (sp->s_block_size % V1_INODE_SIZE) != 0) {
+    return EINVAL;
+  }
+
+  sp->s_isearch = 0;		/* inode searches initially start at 0 */
+  sp->s_zsearch = 0;		/* zone searches initially start at 0 */
+  sp->s_version = version;
+  sp->s_native  = native;
+
+  /* Make a few basic checks to see if super block looks reasonable. */
+  if (sp->s_imap_blocks < 1 || sp->s_zmap_blocks < 1
+      || sp->s_ninodes < 1 || sp->s_zones < 1
+      || (unsigned) sp->s_log_zone_size > 4) {
+    printf("not enough imap or zone map blocks, \n");
+    printf("or not enough inodes, or not enough zones, "
+	   "or zone size too large\n");
+    return(EINVAL);
+  }
+  sp->s_dev = dev;		/* restore device number */
+  return(OK);
+}
+
 void getsuper(char *device){
   /* Open device and set variables for readblock. */
   if ((rawfd= open(rawdev= device, O_RDWR)) < 0) fatal(device);
+
+  /* Initialize variables, return size of file system in blocks,
+   * (zero on error).
+   */
+  /* Read superblock. (The superblock is always at 1kB offset,
+   * that's why we lie to readblock and say the block size is 1024
+   * and we want block number 1 (the 'second block', at offset 1kB).)
+   */
+  readblock(1, scratch, 1024);
+  memcpy(&super, scratch, sizeof(super));
+
+
 }
+
+
 
 int main(int argc, char *argv[]) {
   char image[50]="zj.img";
--- linuxmanaual/src/minix/testfs.h
+++ linuxmanaual/src/minix/testfs.h
@@ -10,8 +10,16 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#define OK            0
+#define BYTE            0377
+#define FALSE 0
+
 /* types */
 #define time_t long
+
+typedef unsigned char   u8_t;	   /* 8 bit type */
+typedef unsigned short u16_t;	   /* 16 bit type */
+typedef unsigned long  u32_t;	   /* 32 bit type */
 
 typedef short          mdev_t;
 typedef unsigned long  mino_t;
@@ -24,10 +32,69 @@ typedef char           mgid_t;
 typedef short        mnlink_t;
 typedef unsigned long moff_t;
 
+#define usizeof(t) ((unsigned) sizeof(t))
+
+/* fs  */
+#define V1		   1	/* version number of V1 file systems */ 
+#define V2		   2	/* version number of V2 file systems */ 
+#define V3		   3	/* version number of V3 file systems */ 
+
+#define NO_DEV                  ((mdev_t) 0)	/* absence of a device numb */
+
+#define SUPER_SIZE      usizeof (struct super_block)  /* super_block size    */
+
+#define SUPER_MAGIC   0x137F	/* magic number contained in super-block */
+#define SUPER_V1 SUPER_MAGIC		/* V1 magic has a weird name. */
+#define SUPER_V2      0x2468
+#define SUPER_V2_REV  0x6824
+#define SUPER_V3      0x4d5a
+
+#define STATIC_BLOCK_SIZE	1024
+#define MIN_BLOCK_SIZE		 1024
+#define MAX_BLOCK_SIZE		 4096
+
 #define NR_SUPERS          8
 #define NR_INODES         64
-#define V2_NR_TZONES      10
 
+#define V1_NR_TZONES       9	/* total # zone numbers in a V1 inode */
+/* Declaration of the V1 inode as it is on the disk (not in core). */
+typedef struct {		/* V1.x disk inode */
+  mmode_t d1_mode;		/* file type, protection, etc. */
+  muid_t d1_uid;			/* user id of the file's owner */
+  moff_t d1_size;		/* current file size in bytes */
+  time_t d1_mtime;		/* when was file data last changed */
+  u8_t d1_gid;			/* group number */
+  u8_t d1_nlinks;		/* how many links to this file */
+  u16_t d1_zone[V1_NR_TZONES];	/* block nums for direct, ind, and dbl ind */
+} d1_inode;
+
+#define V2_NR_TZONES      10
+/* Declaration of the V2 inode as it is on the disk (not in core). */
+typedef struct {		/* V2.x disk inode */
+  mmode_t d2_mode;		/* file type, protection, etc. */
+  u16_t d2_nlinks;		/* how many links to this file. HACK! */
+  muid_t d2_uid;			/* user id of the file's owner. */
+  u16_t d2_gid;			/* group number HACK! */
+  moff_t d2_size;		/* current file size in bytes */
+  time_t d2_atime;		/* when was file data last accessed */
+  time_t d2_mtime;		/* when was file data last changed */
+  time_t d2_ctime;		/* when was inode data last changed */
+  zone_t d2_zone[V2_NR_TZONES];	/* block nums for direct, ind, and dbl ind */
+} d2_inode;
+
+#define V1_INODE_SIZE             usizeof (d1_inode)  /* bytes in V1 dsk ino */
+#define V1_ZONE_NUM_SIZE           usizeof (zone1_t)  /* # bytes in V1 zone  */
+#define V1_INODES_PER_BLOCK (STATIC_BLOCK_SIZE/V1_INODE_SIZE)
+#define V1_NR_DZONES       7
+#define V1_INODES_PER_BLOCK (STATIC_BLOCK_SIZE/V1_INODE_SIZE)
+#define V1_INDIRECTS (STATIC_BLOCK_SIZE/V1_ZONE_NUM_SIZE) 
+
+
+#define V2_NR_DZONES       7
+#define V2_INODE_SIZE             usizeof (d2_inode)  /* bytes in V2 dsk ino */
+#define V2_ZONE_NUM_SIZE            usizeof (zone_t)  /* # bytes in V2 zone  */
+#define V2_INDIRECTS(b)   ((b)/V2_ZONE_NUM_SIZE)
+#define V2_INODES_PER_BLOCK(b) ((b)/V2_INODE_SIZE)/* # V2 dsk inodes/blk */
 
 struct super_block {
   mino_t s_ninodes;		/* # usable inodes on the minor device */
@@ -91,11 +158,21 @@ struct inode {
   char i_update;		/* the ATIME, CTIME, and MTIME bits are here */
 } inode[NR_INODES];
 
+
 int rawfd;	/* File descriptor to open device. */
 char *rawdev;	/* Name of device. */
 
+static struct super_block super;	/* Superblock of file system */
+static char dirbuf[MAX_BLOCK_SIZE];	/* Scratch/Directory block. */
+
+#define scratch dirbuf
 
 void report(char *label);
 void fatal(char *label);
+
+#define BYTE_SWAP          0	/* tells conv2/conv4 to swap bytes */
+unsigned conv2(int norm,int w);
+long conv4(int norm,long x);
+void readblock(moff_t blk, char *buf, int block_size);
 
 #endif	/* __testfs_h */
