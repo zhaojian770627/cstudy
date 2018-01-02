29eadba048afdd9d290d291ee8a090fa25081755
zhaojianc <zhaojianc@yonyou.com>
2018-01-02 13:51:50 +0800

    add

--- linuxmanaual/src/minix/testsize.c
+++ linuxmanaual/src/minix/testsize.c
@@ -11,6 +11,8 @@
 #define CHARMAX 1024
 #define SECTOR_SIZE	512	/* Disk sector size. */
 #define align(n)	(((n) + ((SECTOR_SIZE) - 1)) & ~((SECTOR_SIZE) - 1))
+#define dma_bytes_left(phys)    \
+   ((unsigned) (sizeof(int) == 2 ? 0 : 0x10000) - (unsigned) ((phys) & 0xFFFF))
 
 int main(int argc, char *argv[]) {
   printf("1- %d\n",align(1));
@@ -19,5 +21,7 @@ int main(int argc, char *argv[]) {
   printf("513- %d\n",align(513));
   printf("1024- %d\n",align(1024));
   printf("1025- %d\n",align(1025));
+  printf("sizeof(int)- %ld\n",sizeof(int));
+  printf("dma_bytes_left- %d\n",dma_bytes_left(0));
   exit(EXIT_SUCCESS);
 }
