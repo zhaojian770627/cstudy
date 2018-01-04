4ed21f4a4aaa35007eb743934fbf1a5613e506fa
zhaojianc <zhaojianc@yonyou.com>
2018-01-04 13:50:44 +0800

    add

--- linuxmanaual/src/minix/cvul64m.c
+++ linuxmanaual/src/minix/cvul64m.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef unsigned long  u32_t;	   /* 32 bit type */
 typedef struct { u32_t _[2]; } u64_t;
 
@@ -8,5 +11,7 @@ int main(int argc, char *argv[]) {
   unsigned long l1=20;
   unsigned long l2=40;
   u64=cvul64(l1,l2);
+  printf("_0:%ld\n",u64._[0]);
+  printf("_1:%ld\n",u64._[1]);
   return 0;
 }
