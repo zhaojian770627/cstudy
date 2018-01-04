7ed6aa34698d9c4b8c9dddd2c8c3b3e057bfe7bd
zhaojianc <zhaojianc@yonyou.com>
2018-01-04 13:39:45 +0800

    add

---
+++ linuxmanaual/src/minix/hello.s
@@ -0,0 +1,17 @@
+    section .data
+msg:
+    db "hello, world", 10
+len equ $-msg
+
+    section .text
+    global main
+main:
+    mov edx, len
+    mov ecx, msg
+    mov ebx, 1
+    mov eax, 4 
+    int 0x80
+
+    mov ebx, 0
+    mov eax, 1
+    int 0x80
