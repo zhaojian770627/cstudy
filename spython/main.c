3338b08e99a063ec4129396acb883dc7f91116e6
zhaojian770627 <zhaojian770627@163.com>
2018-11-28 22:49:28 +0800

    add

--- spython/main.c
+++ spython/main.c
@@ -1,7 +1,10 @@
 #include "object.h"
+#include "intobject.h"
 
 int
 main(int argc,char *argv[])
 {
+  PyIntObject* o=(PyIntObject*)PyInt_Create(11);
+  o->type->print((PyObject*)o); 
   printf("abcd");
 }
